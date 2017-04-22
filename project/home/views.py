80144b155fde5c6a407ae6e6ddfc316a7028b1e0
saifraider <saifali120493@gmail.com>
2017-04-22 08:16:58 -0700

    implemented ajax deletion and server side filter in JQuery Datatable. Modified tasks.txt

--- project/home/views.py
+++ project/home/views.py
@@ -6,7 +6,8 @@ from flask import request
 from datatables import ColumnDT, DataTables
 from project.models.users import User
 from flask import jsonify
-from project import db   # pragma: no cover
+from project import db  # pragma: no cover
+
 # from project.models import BlogPost   # pragma: no cover
 
 home_blueprint = Blueprint('home', __name__, template_folder='templates', static_folder='static')
@@ -54,6 +55,7 @@ def ajax():
     print(json.dumps(list_of_users))
     return json.dumps(list_of_users)
 
+
 @home_blueprint.route('/datatable')
 def data():
     """Return server side data."""
@@ -74,6 +76,44 @@ def data():
 
     # instantiating a DataTable for the query and table needed
     rowTable = DataTables(params, query, columns)
+    print(json.dumps(rowTable.output_result()))
+    # returns what is needed by DataTable
+    return jsonify(rowTable.output_result())
 
+
+@home_blueprint.route('/datatable_delete')
+def delete():
+    user_id = request.args['id']
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return json.dumps({"res": "True"})
+
+
+@home_blueprint.route('/filter_id')
+def filter_id():
+    min_val = request.args['min']
+    max_val = request.args['max']
+    print min_val
+    print max_val
+
+    columns = [
+        ColumnDT(User.id),
+        ColumnDT(User.username),
+        ColumnDT(User.email)
+
+    ]
+
+    # defining the initial query depending on your purpose
+    # query = User.query.with_entities(User.id, User.username, User.email).filter(User.id >= min_val).filter(User.id <= max_val))
+    query = db.session.query(User.id,User.username,User.email).filter(User.id >= min_val, User.id <= max_val)
+    print(query)
+    # GET parameters
+    params = request.args.to_dict()
+
+    # instantiating a DataTable for the query and table needed
+    rowTable = DataTables(params, query, columns)
+    print(json.dumps(rowTable.output_result()))
+    # print(json.dumps(rowTable.output_result()))
     # returns what is needed by DataTable
     return jsonify(rowTable.output_result())
